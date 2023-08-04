083c8fc2d8822a9a3c4765027073e1c9be77dea2
Lucas Zemlin <lzemlin@LABA.PSWAS.LOCAL>
2023-08-04 11:54:20 -0700

    finished assignment

--- conditionalsLab.js
+++ conditionalsLab.js
@@ -1,39 +1,44 @@
-//task 1
+// task 1-----------------------------------------------
 function one() {
     let numberOfStudents = 17;
 
     if (numberOfStudents > 20) {
-        console.log("classroom too small")
+        console.log("classroom too small\n")
     } else {
-        console.log("classroom size acceptable")
+        console.log("classroom size acceptable\n")
     }
 }
+one();
 
-//task 2
+
+// task 2-----------------------------------------------
 function two() {
     let numberOfHouses = 5;
 
     if (numberOfHouses > 2) {
-        console.log("you need to sell more houses")
+        console.log("you need to sell more houses\n")
     } else {
-        console.log("seems reasonable")
+        console.log("seems reasonable\n")
     }
 }
+two();
 
-// task 3
+
+// task 3-----------------------------------------------
 function three() {
     let bill = 20;
 
     if (bill > 15) {
-        console.log("we should split the bill")
+        console.log("we should split the bill\n")
     } else {
-        console.log("I'll get the bill this time")
+        console.log("I'll get the bill this time\n")
     }
 }
+three();
 
-//task 4
+// task 4-----------------------------------------------
 function four() {
-    let studentsGrade;
+    let studentsGrade = 88;
     let message;
 
     if (studentsGrade <= 55) {
@@ -50,28 +55,46 @@ function four() {
         message = "over 90! Great!";
     }
 
-    console.log(message);
+    console.log(message + "\n");
 }
+four();
 
-//task 5
+// task 5-----------------------------------------------
 function five() {
     //example 1 - Are you speeding?
-    let driverSpeed;
-    if (driverSpeed > 60){
-        console.log("You are getting a ticket on the highway")
+    let driverSpeed = 45;
+    if (driverSpeed > 60) {
+        console.log("You are getting a ticket on the highway\n")
     } else if (driverSpeed > 35) {
-        console.log("You're getting a ticket on a main road")
+        console.log("You're getting a ticket on a main road\n")
     } else {
-        console.log("You're probably not getting a speeding ticket")
+        console.log("You're probably not getting a speeding ticket\n")
     }
 
-    //example 2 - Check a userName
+    //example 2 - Check a userName-----------------------------------------------
     let userName = "Johnny SilverHand";
 
-    if(userName.toLocaleLowerCase == userName.toLocaleLowerCase) {
-        console.log("You Probably play CyberPunk :)")
+    if (userName.toLocaleLowerCase == userName.toLocaleLowerCase) {
+        console.log("You Probably play CyberPunk :)\n")
     }
-    
 
-    //example 3 - 
+
+    //example 3 - Livin' In America...------------------------------------------- 
+
+    let age = 25;
+    let location = "America"
+
+    if (age < 18 && location != "Europe") {
+        console.log("You can drive, but no drinking or smoking\n")
+    }
+    if (age >= 18
+        && age < 21
+        && location != "Europe"
+    ) {
+        console.log("You can drive, you can smoke, but you can't drink!\n")
+    } else if (age >=21 ) {
+        console.log("You can drive, you can smoke, and you can drink... \njust not all at the same time")
+    }
+
 }
+five();
